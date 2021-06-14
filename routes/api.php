5809196b8e7f2b7145db1a9f235cb13c2644298a
anildadhich024 <Jatinyadav605@gmail.com>
2021-06-14 19:44:16 +0530

    Customer API

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ApiController\CustomerApi\CustomerController;
-use App\Http\Controllers\ApiController\CustomerApi\ProductsController;
-use App\Http\Controllers\ApiController\CustomerApi\OrderController;
-use App\Http\Controllers\ApiController\CustomerApi\VendorController;
-use App\Http\Controllers\ApiController\DriverApi\DriverController;
-use App\Http\Controllers\ApiController\DriverApi\OrderController as DriverOrder;
+use App\Http\Controllers\ApiController\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,43 +21,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 Route::namespace('ApiController')->group(function () {
-    Route::namespace('CustomerApi')->prefix('customer')->group(function () {
-        Route::post('create_account', [CustomerController::class, 'CreateAcount']);
-        Route::post('resend_otp', [CustomerController::class, 'ResendOTP']);
-        Route::post('login_account', [CustomerController::class, 'LoginAcount']);
-        Route::post('customer_profile', [CustomerController::class, 'CustomerProfile']);
-        Route::post('create_address', [CustomerController::class, 'CreateAddress']);
-        Route::post('address_list', [CustomerController::class, 'AddressList']);
-        Route::post('address_delete', [CustomerController::class, 'AddressDelete']);
-        Route::post('update_address', [CustomerController::class, 'UpdateAddress']);
-        Route::post('update_profile_image', [CustomerController::class, 'UpdateProfileImage']);
-        Route::post('feedback', [CustomerController::class, 'SaveFeedback']);
-
-        Route::post('home', [ProductsController::class, 'HomePage']);
-        Route::get('category_list', [ProductsController::class, 'CategoryList']);
-        
-        Route::post('vendor_list', [VendorController::class, 'VendorList']);
-
-        Route::post('place_order', [OrderController::class, 'PlaceOrder']);
-        Route::post('order_list', [OrderController::class, 'OrderList']);
-        Route::post('order_detail', [OrderController::class, 'OrderDetail']);
-
-        Route::post('apply_coupon', [OrderController::class, 'ApplyCoupon']);
-
-        Route::get('cancel_reason', [OrderController::class, 'CancelReason']);
-    });
-
-    Route::namespace('DriverApi')->prefix('driver')->group(function () {
-        Route::post('resend_otp', [DriverController::class, 'ResendOTP']);
-        Route::post('login_account', [DriverController::class, 'LoginAcount']);
-        Route::post('driver_profile', [DriverController::class, 'DriverProfile']);
-        Route::post('driver_token', [DriverController::class, 'UpdateToken']);
-        Route::post('driver_status', [DriverController::class, 'ChangeStatus']);
-
-        Route::post('assign_order', [DriverOrder::class, 'AssignOrder']);
-        Route::post('accept_reject_order', [DriverOrder::class, 'AcceptRejectOrder']);
-        Route::post('ongoing_order', [DriverOrder::class, 'OngoingOrder']);
-        Route::post('completed_order', [DriverOrder::class, 'CompletedOrder']);
-        Route::post('complete_order', [DriverOrder::class, 'CompleteOrder']);
-    });
+        Route::post('register_otp', [UserController::class, 'register_otp']);
+        Route::post('register_account', [UserController::class, 'register_account']);
+        Route::post('update_account', [UserController::class, 'update_account']);
+        Route::post('login_account', [UserController::class, 'login_account']);
+        Route::post('resend_otp', [UserController::class, 'resend_otp']);
+        Route::post('profile', [UserController::class, 'user_profile']);
 });
